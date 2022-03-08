29a069479a55b45b3f444c9e4d0db122a63566f9
spiralspree <spiralspree@gmail.com>
2022-03-08 19:57:06 +0100

    Call a DataBaseManager's getDataSource method in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        DataBaseManager dataBaseManager = new DataBaseManager();
+        dataBaseManager.getDataSource();
     }
 }
