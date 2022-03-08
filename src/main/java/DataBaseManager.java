d61be4b7108f50bbb9053c3ed570cf4bcd702758
spiralspree <spiralspree@gmail.com>
2022-03-08 19:56:04 +0100

    Create DataBaseManager and its getDataSource method

---
+++ src/main/java/DataBaseManager.java
@@ -0,0 +1,25 @@
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class DataBaseManager {
+
+    public DataSource getDataSource() {
+        PGSimpleDataSource source = new PGSimpleDataSource();
+        source.setServerNames(new String[] {System.getenv("WANNAJOIN_DB_SERVER")});
+        source.setDatabaseName(System.getenv("WANNAJOIN_DB_NAME"));
+        source.setPortNumbers(new int[] {Integer.parseInt(System.getenv("WANNAJOIN_DB_PORT"))});
+        source.setUser(System.getenv("WANNAJOIN_DB_USER"));
+        source.setPassword(System.getenv("WANNAJOIN_DB_PW"));
+        source.setSslMode("require");
+        try {
+            System.out.println("Data source parameters set. Testing connection...");
+            source.getConnection().close();
+            System.out.println("Connection test was successful.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return source;
+    }
+}
