5746b5410f96d5931a0388937e3a616a575c4662
spiralspree <spiralspree@gmail.com>
2022-03-16 20:36:26 +0100

    Close connection and use logger instead of 'shout'.

--- src/main/java/TestTableDAO.java
+++ src/main/java/TestTableDAO.java
@@ -3,26 +3,28 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class TestTableDAO {
     private final DataSource dataSource;
+    private final static Logger LOGGER = Logger.getLogger(CustomLogger.class.getName());
 
     public TestTableDAO (DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-
     public String getTextById (int id) throws SQLException {
-        System.out.println("DAO preparing request: getTextById");
+        LOGGER.info("DAO preparing request: getTextById");
         Connection con = dataSource.getConnection();
         String query = "SELECT text FROM test_table WHERE id = ?";
         PreparedStatement statement = con.prepareStatement(query);
         statement.setInt(1, id);
         ResultSet resultSet = statement.executeQuery();
+        con.close();
         if(!resultSet.next()){
             return null;
         }
-        System.out.println("Record found.");
+        LOGGER.info("Record found.");
         return resultSet.getString(1);
     }
 }
