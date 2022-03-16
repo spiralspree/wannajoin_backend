61950b4b240b06adaff9a8d41c1a928799e5f601
spiralspree <spiralspree@gmail.com>
2022-03-16 20:34:14 +0100

    Create CustomLogger class.

---
+++ src/main/java/CustomLogger.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.logging.*;
+import java.util.logging.Logger;
+
+public class CustomLogger {
+
+    public static void setup(String fileNameText, Formatter chosenFormatter) throws IOException {
+        Logger logger = Logger.getLogger(CustomLogger.class.getName());
+        logger.setLevel(Level.INFO);
+
+        FileHandler handler = new FileHandler(fileNameText);
+        handler.setFormatter(chosenFormatter);
+        logger.addHandler(handler);
+    }
+}
