e17f294b81ea4838749c94e5a6f18d73522c5e1d
spiralspree <spiralspree@gmail.com>
2022-06-27 19:20:48 +0200

    Set header to allow CORS in TestController

--- src/main/java/com/spiralspree/wannajoin/TestController.java
+++ src/main/java/com/spiralspree/wannajoin/TestController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/")
 public class TestController {
 
     @GetMapping("")
-    public String getHome() {
+    public String getHome(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "https://sharp-carson-94751d.netlify.app");
         return "Hello from TestController";
     }
 }
