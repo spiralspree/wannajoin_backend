1cae502ef4e145e8692b6cea32689c21342432b3
spiralspree <spiralspree@gmail.com>
2022-07-21 21:23:22 +0200

    Add lombok getters and setters to User fields

--- src/main/java/com/spiralspree/wannajoin/models/User.java
+++ src/main/java/com/spiralspree/wannajoin/models/User.java
@@ -1,10 +1,16 @@
 package com.spiralspree.wannajoin.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 
 // TODO: implement Lombok for getters and reasonable setters
 public class User {
+
+    @Getter
     private final String name;
+    @Getter @Setter
     private ArrayList<User> friends = new ArrayList<>();
 
     public User(String name) {
