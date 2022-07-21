3b04e2a8ae4b4b27e95be0151952f79c9636bc1a
spiralspree <spiralspree@gmail.com>
2022-07-21 21:09:29 +0200

    Add lombok getters and setters to Plan fields

--- src/main/java/com/spiralspree/wannajoin/models/Plan.java
+++ src/main/java/com/spiralspree/wannajoin/models/Plan.java
@@ -1,15 +1,23 @@
 package com.spiralspree.wannajoin.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Date;
 
 // TODO: implement Lombok for getters and reasonable setters
 public class Plan {
 
+    @Getter @Setter
     private String title;
+    @Getter
     private final User host;
+    @Getter @Setter
     private ArrayList<User> participants = new ArrayList<>();
+    @Getter @Setter
     private Date dueDate;
+    @Getter @Setter
     private boolean isAchieved = false;
 
     public Plan(String title, User host) {
