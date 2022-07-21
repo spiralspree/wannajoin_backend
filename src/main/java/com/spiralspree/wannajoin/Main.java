0b9eff2db2366db470fd8cbff69b234bf252c2d6
spiralspree <spiralspree@gmail.com>
2022-07-22 00:14:56 +0200

    Remove database test code from Main

--- src/main/java/com/spiralspree/wannajoin/Main.java
+++ src/main/java/com/spiralspree/wannajoin/Main.java
@@ -3,12 +3,7 @@ package com.spiralspree.wannajoin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
@@ -25,23 +20,5 @@ public class Main {
             e.printStackTrace();
         }
         LOGGER.info("Application started");
-
-        DataBaseManager dataBaseManager = new DataBaseManager();
-        DataSource dataSource = dataBaseManager.getDataSource();
-        TestTableDAO testTableDAO = new TestTableDAO(dataSource);
-
-        String retrievedText;
-        try {
-            retrievedText = testTableDAO.getTextById(1);
-            LOGGER.info("Query result: \"" + retrievedText + "\"");
-        } catch (SQLException e) {
-            LOGGER.severe("DAO action failed.");
-        }
-
-        try {
-            testTableDAO.addTextToTestTable();
-        } catch (SQLException e) {
-            LOGGER.severe("DAO action failed.");
-        }
     }
 }
