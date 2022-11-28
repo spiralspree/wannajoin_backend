8e1f3acc85c506d0f6a829d1474fd6c117fa94d9
spiralspree <spiralspree@gmail.com>
2022-11-28 19:28:49 +0100

    Add Logs.txt.lck to .gitignore

---
+++ src/main/java/com/spiralspree/wannajoin/models/repositories/HbrntTestClassRepo.java
@@ -0,0 +1,7 @@
+package com.spiralspree.wannajoin.models.repositories;
+
+import com.spiralspree.wannajoin.models.HbrntTestClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HbrntTestClassRepo extends JpaRepository<HbrntTestClass, Long> {
+}
